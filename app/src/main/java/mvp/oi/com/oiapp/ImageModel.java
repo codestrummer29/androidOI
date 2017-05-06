6572a0a03ab68a6acaa3d3bad4bb841f271f8fd3
Saahil <saahil2910@gmail.com>
2017-05-06 13:51:46 +0530

    Recycler View with add images from gallery

---
+++ app/src/main/java/mvp/oi/com/oiapp/ImageModel.java
@@ -0,0 +1,19 @@
+package mvp.oi.com.oiapp;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Saahil on 06/05/17.
+ */
+
+public class ImageModel {
+    Bitmap image;
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+}
